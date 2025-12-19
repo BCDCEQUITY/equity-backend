1e4a8059e91616c571ac05c41f8a6e133861130f
BCDCEQUITY <equitybcdcsecurite@outlook.com>
2025-12-19 12:50:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json()); // bodyParser non nécessaire
 
 // Connexion à MongoDB Atlas
